b17c9db836ff5b64d552678c590d51dc73c4b40b
Propt Development <proptdevelopment@Propts-MacBook-Air.local>
2013-02-18 22:15:47 -0500

    Initial Commit

---
+++ Fresh_Start/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  Fresh_Start
+//
+//  Created by Propt Development on 2/18/13.
+//  Copyright (c) 2013 Propt Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navController;
+
+@end
+
+
---
+++ Fresh_Start/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Fresh_Start
+//
+//  Created by Propt Development on 2/18/13.
+//  Copyright (c) 2013 Propt Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
